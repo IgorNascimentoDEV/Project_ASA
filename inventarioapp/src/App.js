38e870c086a0c474f167a77c3bb9ae15fb5bbab6
igor <diegoigor2001@gmail.com>
2023-05-17 22:54:32 -0300

    Començando o projeto em React

---
+++ inventarioapp/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Home from './components/Sobre';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Minha aplicação em React</h1>
+      <Home />
+    </div>
+  );
+}
+
+export default App;
